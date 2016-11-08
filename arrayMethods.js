03821fc90fdd729c85745cce7ecbce81c822282e
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-11-07 16:37:09 -1000

    finisheddddd

--- arrayMethods.js
+++ arrayMethods.js
@@ -61,7 +61,7 @@ var myFruits = fruitCollection.join(" + ");
 
 var favoriteFriends = friends.slice(1, 3);
 
-//var owesMoney = friends.slice(1, 4);
+var owesMoney = friends.slice(3, 6);
 
 var monthNameString = monthNames.toString();
 
@@ -69,7 +69,7 @@ var favoriteColor = colors.indexOf("Red");
 
 var favoriteEvenNumber = mixedNums.indexOf(76);
 
-var lastNine =bulkNumbers.lastIndexOf(9);
+var lastNine = bulkNumbers.lastIndexOf(9);
 
 var lastFive = bulkNumbers.lastIndexOf(5);
 
